e41708fac97395f78e0be28c5bf02b3ac73a510e
Stijn <stijnmuylle@gmail.com>
2013-06-22 02:03:53 +0200

    changed back to fixtures

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@ App = Ember.Application.create({});
 
 App.Store = DS.Store.extend({
   revision: 12,
-  adapter: DS.RESTAdapter.extend({
-    url: 'http://localhost:3000'
-  })
+  adapter: 'DS.FixtureAdapter'
 });
 
 App.Router.map(function() {
@@ -20,25 +18,11 @@ App.PostsRoute = Ember.Route.extend({
   }
 });
 
-App.PostController = Ember.ObjectController.extend({
-  isEditing: false,
-
-  edit: function() {
-    this.set('isEditing', true);
-  },
-
-  doneEditing: function() {
-    this.set('isEditing', false);
-    this.get('store').commit();
-  }
-});
-
 var attr = DS.attr;
 
 App.Post = DS.Model.extend({
   title: attr('string'),
   author: attr('string'),
-  intro: attr('string'),
   extended: attr('string'),
   publishedAt: attr('date')
 });
@@ -52,3 +36,28 @@ Ember.Handlebars.registerBoundHelper('markdown', function(input) {
 Ember.Handlebars.registerBoundHelper('date', function(date) {
   return moment(date).fromNow();
 });
+
+
+App.Post.FIXTURES = [
+{
+    id: 1,
+    title: "bliep",
+    author: "ddccffvv",
+    extended: "blablabla",
+    publishedAt: "2013-06-21"
+},
+{
+    id: 2,
+    title: "Vim and binary files",
+    author: "ddccffvv",
+    extended: "    :%!xxd\n    :%!xxd -r",
+    publishedAt: "2013-06-21"
+},
+{
+    id: 3,
+    title: "Flask and apache",
+    author: "ddccffvv",
+    extended: "wsgi config put the webapp in a subfolder\nTo put in the sites enabled config:\n    WSGIDaemonProcess test user=usr group=usr home=\"/var/www/site/directory\"\n    WSGIScriptAlias /path/on/site \"/path/to/wsgi/file/test.wsgi\"    <Directory /var/www/site/directory/>    WSGIProcessGroup test\n    WSGIApplicationGroup %{GLOBAL}\n    WSGIScriptReloading On # Automatic reload if the plugin files are changed\n    WSGIPassAuthorization On # pass on http authorization to the application\n    Order allow,deny\n    Allow from all\n    </Directory>",
+    publishedAt: "2013-06-21"
+},
+];
