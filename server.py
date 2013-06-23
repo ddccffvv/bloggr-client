16778e9ec7fe67040f0c0a7fda0addc225fe8f33
Stijn <stijnmuylle@gmail.com>
2013-06-23 13:22:19 +0200

    using ember data, works with flask

--- server.py
+++ server.py
@@ -2,25 +2,42 @@ from flask import Flask, Response
 import json
 app = Flask(__name__)
 
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
 @app.route('/posts')
-def index():
+def posts():
     return Response(json.dumps({"posts": get_posts()}), mimetype='application/json')
+
+@app.route('/posts/<postid>')
+def postsid(postid):
+    print postid
+    posts = get_posts()
+    try:
+        return Response(json.dumps({"post": posts[int(postid)-1]}), mimetype='application/json')
+    except Exception:
+        print "something went wrong"
+        return Response(json.dumps({"post": []}), mimetype='application/json')
+
 
 
 def get_posts():
     import os
     posts = [] 
-    for files in os.listdir("posts"):
-        post = {}
-        post["id"] = files
-        lines = open("posts/" + files, "r").readlines()
-        post["title"] = lines[0].rstrip()
-        post["author"] = lines[1].rstrip()
-        post["publishedAt"] = lines[2].rstrip()
-        post["extended"] = ''.join(lines[3:])
-        posts.append(post)
+    for files in sorted(os.listdir("posts"), reverse=True):
+        if files.endswith(".post"):
+            print files
+            post = {}
+            post["id"] = files.split(".")[0]
+            lines = open("posts/" + files, "r").readlines()
+            post["title"] = lines[0].rstrip()
+            post["author"] = lines[1].rstrip()
+            post["publishedAt"] = lines[2].rstrip()
+            post["extended"] = ''.join(lines[3:])
+            posts.append(post)
     return posts
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
